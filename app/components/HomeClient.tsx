dd8070ae2273cac1ecf9a8569003ecf5aa6c3ef3
codesyariah122 <pujiermanto@gmail.com>
2025-02-06 00:36:00 +0700

    fix/move-another-components-client

---
+++ app/components/HomeClient.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import { HomePage } from "@pages/index";
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+  DehydratedState,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function HomeClient({
+  dehydratedState,
+}: {
+  dehydratedState: DehydratedState;
+}) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-full">
+          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
+            <HomePage />
+          </main>
+        </div>
+      </Hydrate>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,12 @@
-"use client";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+import HomeClient from "@components/HomeClient";
+export default async function Home() {
+  const queryClient = new QueryClient();
 
-import { useState } from "react";
-import { HomePage } from "@pages/index";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Hydrate, DehydratedState } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+  // Prefetch data jika perlu
+  // await queryClient.prefetchQuery(["someKey"], fetchDataFunction);
 
-export default function Home({
-  dehydratedState,
-}: {
-  dehydratedState: DehydratedState;
-}) {
-  const [queryClient] = useState(() => new QueryClient());
+  const dehydratedState = dehydrate(queryClient);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {/* Hydrate digunakan untuk memulihkan state dari SSR/SSG */}
-      <Hydrate state={dehydratedState}>
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-full">
-          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
-            <HomePage />
-          </main>
-        </div>
-      </Hydrate>
-
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <HomeClient dehydratedState={dehydratedState} />;
 }
